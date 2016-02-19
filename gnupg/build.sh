215d51487872a07686c10ce119263ad0be7bb50d
Shun Ikejima <syun@ikejima.org>
2016-02-19 01:50:41 -0800

    Add pcsc-lite and ccid to gnupg package.

--- gnupg/build.sh
+++ gnupg/build.sh
@@ -3,7 +3,7 @@ cd `dirname $0`
 
 cp optional/* /tmp/tce/optional
 
-LIBS='compiletc make openssl ncurses-dev libgpg-error-dev libgcrypt-dev libassuan-dev libksba-dev'
+LIBS='compiletc make openssl ncurses-dev libgpg-error-dev libgcrypt-dev libassuan-dev libksba-dev libusb-dev perl5'
 tce-load -w $LIBS
 tce-load -i -c $LIBS
 
@@ -14,6 +14,8 @@ PKG=gnupg-2.0.29
 if [ ! -f $PKG.tar.bz2 ]; then
   wget https://www.gnupg.org/ftp/gcrypt/gnupg/gnupg-2.0.29.tar.bz2
   wget ftp://ftp.gnu.org/gnu/pth/pth-2.0.7.tar.gz
+  wget http://ftp.internat.freebsd.org/pub/FreeBSD/distfiles/ccid-1.4.21.tar.bz2
+  wget http://ftp.internat.freebsd.org/pub/FreeBSD/distfiles/pcsc-lite-1.8.15.tar.bz2
 fi
 md5sum -c pkg.md5
 if [ $? -eq 1 ]; then
@@ -23,6 +25,32 @@ fi
 
 cd /tmp
 
+rm -rf pcsc-lite-1.8.15
+
+tar xvf `dirname $0`/pcsc-lite-1.8.15.tar.bz2
+
+cd pcsc-lite-1.8.15
+
+./configure
+mkdir -p /tmp/gnupg
+sudo make install
+make DESTDIR=/tmp/gnupg install
+
+sh
+cd /tmp
+rm -rf ccid-1.4.21
+
+tar xvf `dirname $0`/ccid-1.4.21.tar.bz2
+
+cd ccid-1.4.21
+
+./configure
+mkdir -p /tmp/gnupg
+sudo make install
+make DESTDIR=/tmp/gnupg install
+
+sh
+cd /tmp
 rm -rf pth-2.0.7
 
 tar xvf `dirname $0`/pth-2.0.7.tar.gz
@@ -60,6 +88,7 @@ rm -r /tmp/gnupg/usr/local/man
 rm -r /tmp/gnupg/usr/local/lib/libpth.la
 rm -r /tmp/gnupg/usr/local/lib/libpth.a
 
+rm gnupg.tcz
 mksquashfs /tmp/gnupg gnupg.tcz
 
 echo done
